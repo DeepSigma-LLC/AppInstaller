1a56256e273c61b43416b6552a1612c07da9fbc1
Brenden Perry <brendenperry5@gmail.com>
2025-07-27 16:57:48 -0700

    Enhance log display with RichEditBox and color output
    Replaces the log TextBox with a RichEditBox to support colored log messages. Implements asynchronous log updates with color-coded status messages and hides the progress bar upon completion. Adds helper methods for appending colored text and converting color formats.

--- AppInstaller/Views/Main.xaml.cs
+++ AppInstaller/Views/Main.xaml.cs
@@ -1,7 +1,9 @@
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
@@ -13,6 +15,10 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System.Drawing;
+using System.Threading.Tasks;
+using Windows.UI;
+using System.Threading;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,18 +33,69 @@ namespace AppInstaller.Views
         public Main()
         {
             InitializeComponent();
-            UpdateLog();
+            this.Loaded += Main_Loaded;
         }
 
-        private void UpdateLog()
+        private async void Main_Loaded(object sender, RoutedEventArgs e)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.Append("Starting Application Installation...\n");
-            sb.Append("Analzing structure...\n");
-            sb.Append("Updating Application Files...\n");
-            sb.Append("Application Succesfully Updated!\n");
-
-            textbox_log.Text = sb.ToString();
+            await UpdateLogAsync();
         }
+
+        private async Task UpdateLogAsync()
+        {
+            AppendColoredText(textbox_log, "Starting Application Installation... ", System.Drawing.Color.White);
+            await Task.Delay(1000);
+            AppendColoredText(textbox_log, "Successful!\n", System.Drawing.Color.Green);
+
+            AppendColoredText(textbox_log, "Analyzing file structure... ", System.Drawing.Color.White);
+            await Task.Delay(4000);
+            AppendColoredText(textbox_log, "Successful!\n", System.Drawing.Color.Green);
+
+            AppendColoredText(textbox_log, "Deleting old files.. ", System.Drawing.Color.White);
+            await Task.Delay(3000);
+            AppendColoredText(textbox_log, "Successful!\n", System.Drawing.Color.Green);
+
+            AppendColoredText(textbox_log, "Transfering new files... ", System.Drawing.Color.White);
+            await Task.Delay(3000);
+            AppendColoredText(textbox_log, "Successful!\n", System.Drawing.Color.Green);
+
+            AppendColoredText(textbox_log, "\n", System.Drawing.Color.Green);
+
+            AppendColoredText(textbox_log, "Done! ", System.Drawing.Color.Green);
+            AppendColoredText(textbox_log, "This application will close in 5 seconds and relaunch your target application.", System.Drawing.Color.White);
+
+            progressBar.Visibility = Visibility.Collapsed;
+            
+        }
+
+     
+        private void AppendColoredText(RichEditBox box, string text, System.Drawing.Color color)
+        {
+            // Move selection to the end
+            box.Document.GetText(Microsoft.UI.Text.TextGetOptions.None, out string existingText);
+            int length = existingText.Length;
+
+            // Set selection at end of document
+            var doc = box.Document;
+            doc.Selection.SetRange(length, length);
+
+            // Insert text
+            doc.Selection.Text = text;
+
+            // Apply color format to just-inserted text
+            doc.Selection.CharacterFormat.ForegroundColor = ConvertColor(color);
+        }
+
+        private Windows.UI.Color ConvertColor(System.Drawing.Color color)
+        {
+            Windows.UI.Color winUIColor = Windows.UI.Color.FromArgb(
+                color.A,
+                color.R,
+                color.G,
+                color.B);
+            return winUIColor;
+        }
+
+
     }
 }
