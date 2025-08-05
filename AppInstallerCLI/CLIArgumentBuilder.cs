f09822a17ebfe429001b8a6f647f11da4343bdb3
Brenden Perry <brendenperry5@gmail.com>
2025-08-04 18:36:23 -0700

    Update CLIArgumentBuilder.cs

--- AppInstallerCLI/CLIArgumentBuilder.cs
+++ AppInstallerCLI/CLIArgumentBuilder.cs
@@ -24,7 +24,7 @@ namespace AppInstallerCLI
                "Display the current directory of the application.");
             arguments.Add("--path", path);
 
-            arguments.Add("No arguments", new ConsoleArgument(() => {}, "Launchs the UI version of the application.")); //Action does nothing, just launches the UI version.
+            arguments.Add("No Argument", new ConsoleArgument(() => {}, "Launchs the UI version of the application.")); //Action does nothing, just launches the UI version.
 
             return arguments;
         }
